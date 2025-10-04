e440bb9bfd5b8403bf32a9c334f501ee5a0c5b59
darshilbhakhar179@gmail.com <darshilbhakhar179@gmail.com>
2025-10-03 21:58:24 -0400

    gettinng category based questions and adding question using postman/api post

--- src/main/java/com/darshil/quizapp/dao/QuestionDao.java
+++ src/main/java/com/darshil/quizapp/dao/QuestionDao.java
@@ -3,10 +3,13 @@ package com.darshil.quizapp.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.darshil.quizapp.Question;
+import java.util.List;
+
 
 
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer>{
 
+    List<Question> findByCategory(String category);
 
 }
